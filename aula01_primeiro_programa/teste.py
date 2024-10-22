1bffe9c1af5f82f1b85768be8a1f0d30fc0af12f
JM-SILVA-PS <joaomarcosdearaujosilva6@gmail.com>
2024-10-22 16:39:35 -0300

    teste

---
+++ aula01_primeiro_programa/teste.py
@@ -0,0 +1,19 @@
+# Curso de Desenvolvimento de Sistemas
+# Turma 0152 
+#Autor: João Marcos de A.Silva
+#Data: 22/04/2024
+#Primeiro Programa em Python
+
+# Importando as bibliotecas 
+import os
+
+
+# Limpando o terminal
+os.system('cls')
+
+# O comando print() executa uma saida via terminal
+
+print('-' * 70)
+print('Hello World!!!')
+print('-' * 70)
+print()
---
+++ aula02_variaveis/variaveis.py
@@ -0,0 +1,24 @@
+os.system('cls')
+
+print('-'*70)
+print('Estudo de variaveis')
+print('='*70)
+
+nome = 'Cleitin'
+nascimento = 1995
+altura = 1.68
+peso = 50.5
+doador = True
+complexo = 3j
+PI = 3.14
+
+print('\033[0m \033[31mTipos declarados:\033[0m')
+print('\033[0m A var \033[32m nome \033[0m é do tipo:', type(nome)
+print('\033[0m A var \033[32m nascimento \033[0m é do tipo:', type(nascimento)
+print('\033[0m A var \033[32m altura \033[0m é do tipo:', type
+print('\033[0m A var \033[32m peso \033[0m é do tipo:', type
+print('\033[0m A var \033[32m doador \033[0m é do tipo:', type
+print('\033[0m A var \033[32m complexo \033[0m é do tipo:', type
+print('\033[0m A var \033[32m PI
+print('-'*70)
+print('')
