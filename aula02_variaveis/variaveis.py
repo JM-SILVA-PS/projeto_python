59efb15858ccdf09d270738b24bfc13da7b38f0d
JM-SILVA-PS <joaomarcosdearaujosilva6@gmail.com>
2024-10-23 13:07:06 -0300

    whathaver

--- aula02_variaveis/variaveis.py
+++ aula02_variaveis/variaveis.py
@@ -13,12 +13,12 @@ complexo = 3j
 PI = 3.14
 
 print('\033[0m \033[31mTipos declarados:\033[0m')
-print('\033[0m A var \033[32m nome \033[0m é do tipo:', type(nome)
-print('\033[0m A var \033[32m nascimento \033[0m é do tipo:', type(nascimento)
-print('\033[0m A var \033[32m altura \033[0m é do tipo:', type
-print('\033[0m A var \033[32m peso \033[0m é do tipo:', type
-print('\033[0m A var \033[32m doador \033[0m é do tipo:', type
-print('\033[0m A var \033[32m complexo \033[0m é do tipo:', type
-print('\033[0m A var \033[32m PI
+print('\033[0m A var \033[32m nome \033[0m é do tipo:', type(nome))
+print('\033[0m A var \033[32m nascimento \033[0m é do tipo:', type(nascimento))
+print('\033[0m A var \033[32m altura \033[0m é do tipo:', type(altura))
+print('\033[0m A var \033[32m peso \033[0m é do tipo:', type(peso))
+print('\033[0m A var \033[32m doador \033[0m é do tipo:', type(doador))
+print('\033[0m A var \033[32m complexo \033[0m é do tipo:', type(complexo))
+print('\033[0m A var \033[32m PI \033[0m é do tipo:', type(PI))
 print('-'*70)
 print('')
