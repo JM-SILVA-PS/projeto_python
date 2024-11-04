790b22a1438053208aff2b0cd76f0a97199803dc
JM-SILVA-PS <joaomarcosdearaujosilva6@gmail.com>
2024-11-04 16:18:28 -0300

    16;18

--- aula08_funçoes/atividade003/F.py
+++ aula08_funçoes/atividade003/F.py
@@ -5,5 +5,10 @@ os.system('cls')
 
 print('tente pensar o mesmo numero que a maquina ')
 numero= int(input('Qual numero você escolhe '))
-numero_pc =random.randint(0,10)
-print(f'O numero é {numero_pc}')
+numero_pc =random.randint(0,0)
+
+if numero == numero_pc:
+    print(f' parabens você acertouhttps://br.pinterest.com/pin/732116483156685068/')
+
+else:
+    print(f'Errou .muito burro sua mãe deve ter vergonha de você.......ah e o numero é {numero_pc}')
