b1ca5efd17f673275f1f819d3ffa8143482bcc26
JM-SILVA-PS <joaomarcosdearaujosilva6@gmail.com>
2024-11-07 16:42:04 -0300

    07/11/24_2

--- teste.py
+++ teste.py
@@ -1,1 +1,19 @@
-# teste
+#ORDEM DEPRIMENTE 
+import os
+
+os.system('cls')
+
+numa =int(input('primeiro numero: '))
+numb = int(input('segundo numero'))
+numc = int( input('terceiro numero'))
+
+if numa > numb and numa >numc and numb > numc:
+    print(f'{numa},{numb},{numc} ')
+
+elif numa> numb and numa>numc and numc> numb:
+    print(f'{numa},{numc},{numb}')
+
+elif numb > numa and numb > numc and numa > numc:
+    print(f'{numb},{numa},{numc}')
+elif numb > numa and numb > numc and numc > numa:
+    print(f'{numb},{numc},{numa}')
