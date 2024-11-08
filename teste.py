b1b97e979e1ee74b51280d4272d27e6bb64887c5
JM-SILVA-PS <joaomarcosdearaujosilva6@gmail.com>
2024-11-08 15:48:20 -0300

    08/11/24.2

--- teste.py
+++ teste.py
@@ -1,19 +1,50 @@
-#ORDEM DEPRIMENTE 
+
 import os
 
 os.system('cls')
 
-numa =int(input('primeiro numero: '))
-numb = int(input('segundo numero'))
-numc = int( input('terceiro numero'))
+# Escreva um programa que testa se um número é divisível por 2, 3 ou 5. 
+# A única linha dos casos de teste contém um número N 
+# Imprima 3 linhas, na primeira linha escreva S se o número for divisível por 2 e N caso contrário,
+ # na segunda linha faça o mesmo para 3 e na terceira linha para 5.
 
-if numa > numb and numa >numc and numb > numc:
-    print(f'{numa},{numb},{numc} ')
+num =int(input('inira um numero inteiro: '))
 
-elif numa> numb and numa>numc and numc> numb:
-    print(f'{numa},{numc},{numb}')
+if num %2==0 and num %3==0 and num %5==0:
+    print(f'o numero {num} é divisivel por 2')
+    print(f'o numero {num} é divisivel por 3')
+    print(f'o numero {num} é divisivel por 5')
+elif num %2!= 0 and num %3!=0 and num %5==0:
+    print(f'o numero {num} não é divisivel por 2')
+    print(f'o numero{num} não é divisivel por 3')
+    print(f'o numero {num} é divisivel por 5')
+    
+elif num % 2==0 and num %3==0 and num %5!=0:
+    print(f'o numero {num} é divisivel por 2')
+    print(f'o numero {num} é divisivel por 3')
+    print(f'o numero{num} não é divisivel por 5')
 
-elif numb > numa and numb > numc and numa > numc:
-    print(f'{numb},{numa},{numc}')
-elif numb > numa and numb > numc and numc > numa:
-    print(f'{numb},{numc},{numa}')
+elif num % 2==0 and num %3!=0 and num %5==0:
+    print(f'o numero {num} é divisivel por 2')
+    print(f'o numero{num} não é divisivel por 3')
+    print(f'o numero {num} é divisivel por 5')
+elif num %2!= 0 and num %3==0 and num %5!=0:
+    print(f'o numero {num} não é divisivel por 2')
+    print(f'o numero {num} é divisivel por 3')
+    print(f'o numero{num} não é divisivel por 5')
+
+elif num %2!= 0 and num %3==0 and num %5==0:
+    print(f'o numero {num} não é divisivel por 2')
+    print(f'o numero {num} é divisivel por 3')
+    print(f'o numero {num} é divisivel por 5')
+else:
+   
+    print(f'o numero {num} não é divisivel por 2')
+    print(f'o numero{num} não é divisivel por 3')
+    print(f'o numero{num} não é divisivel por 5')
+
+
+    
+
+
+
